d9ef276a9a469d94a7c9b151a2fe06a21e302c3d
KO19N <kolanfoxx@gmail.com>
2024-01-01 13:10:52 +0300

    no movement

--- GameState.cpp
+++ GameState.cpp
@@ -1,6 +1,9 @@
 #include "GameState.h"
 
 GameState::GameState(Lion& lion1, Chick& chick1, Lion& lion2, Chick& chick2) {
+	std::vector<IFigure> x; 
+	figures.push_back(x);
+	figures.push_back(x);
 	figures[0].push_back(lion1);
 	figures[0].push_back(chick1);
 	figures[1].push_back(chick2);
--- main.cpp
+++ main.cpp
@@ -10,29 +10,29 @@ int main()
     Chick chick2(2, 3);
     Lion lion1(2, 1);
     Lion lion2(2, 4);
-    std::cout << 1;
-    //GameState deck(lion1, chick1, lion2, chick2);
-    std::cout << 1.5;
+
+    GameState deck(lion1, chick1, lion2, chick2);
+
     std::string table[4][3]{
         {"O", "O", "O"},
         {"O", "O", "O"},
         {"O", "O", "O"},
         {"O", "O", "O"}
     };
-    std::cout << 2;
-    /*
+
+    
     int x, y;
     for (int i = 0; i < deck.getAll()[0].size(); i++){
         x = deck.getAll()[0][i].getX();
         y = deck.getAll()[0][i].getY();
-        table[x][y] = deck.getAll()[0][i].getName();
+        table[y - 1][x - 1] = deck.getAll()[0][i].getName();
         x = deck.getAll()[1][i].getX();
         y = deck.getAll()[1][i].getY();
-        table[x][y] = deck.getAll()[1][i].getName();
-    }*/
-    std::cout << 3;
+        table[y - 1][x - 1] = deck.getAll()[1][i].getName();
+    }
+
     for (int i = 0; i < 4; i++){
-        for (int j = 0; i < 3; j++){
+        for (int j = 0; j < 3; j++){
             std::cout << table[i][j] << " ";
         }
         std::cout << "\n";
